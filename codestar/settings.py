c64c62dcd64c27c03ec8cee7c9eab0c164a8fecf
JavierBL89 <notri80@gmail.com>
2022-04-05 20:32:10 +0000

    Awards section created

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['one-pico-restaurant.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['one-pico-restaurant.herokuapp.com', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
