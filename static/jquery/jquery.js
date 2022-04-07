62b64e6dd10a596744f9cd91a1292f95f7297279
JavierBL89 <notri80@gmail.com>
2022-04-07 18:44:07 +0000

    Responsive navbar created

---
+++ static/jquery/jquery.js
@@ -0,0 +1,9 @@
+// $(function(){
+//     if($(window).width < 700){
+//         $('ul')[0].removeAttribute("class");
+//         $('body').css("background-color", "blue")
+//         // $('.btn').addClass('backup-btn').removeClass('btn');
+//         // $('.mybtn').addClass('dropdown-toggle').attr("data-toggle", "dropdown");
+//         // $('.myanchor').addClass('anchor');
+//     }
+// });
