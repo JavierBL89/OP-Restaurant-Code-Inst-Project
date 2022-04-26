05bae579bfc2f7238b82281096f2ad09f212a5df
JavierBL89 <notri80@gmail.com>
2022-04-26 01:02:50 +0000

    Validation form in proccess

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -12,29 +12,6 @@ jQuery(function(){
       $(".headding h1").addClass("hover");
       });
 
-    //   $(function(){
-    //     var date_input=$('input[name="date"]'); //our date input has the name "date"
-    //     var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-    //     date_input.datepicker({
-    //         format: 'mm/dd/yyyy',
-    //         container: container,
-    //         todayHighlight: true,
-    //         autoclose: true,
-    //     })
-
-    //     $('#timePicker').clockTimePicker({  
-    //       duration: true,
-    //       durationNegative: true,
-    //      precision: 5,
-    //      i18n: {
-    //       cancelButton: 'Abbrechen'
-    //       },
-    //      onAdjust: function(newVal, oldVal) {
-    //       //...
-    //       }
-    //     });
-    //  });
-
   });
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -3,53 +3,171 @@ new WOW().init()
 const form = document.getElementById('booking_form')
 
 let fname = document.getElementById("name");
-let surname = document.getElementById("lname");
+let surname = document.getElementById("l_name");
 let prefix = document.getElementById("prefix");
 let phone = document.getElementById("phone");
 let email = document.getElementById("email");
 let date = document.getElementById("date");
-let start_time = document.getElementById("timepicker");
+let start_time = document.getElementById("start_time");
 let party_size = document.getElementById("party_size");
 
-function handleSubmit(event){
 
-// const formField = input.parentElement;
+/**
+* INITIALIZE FORM VALIDATION
+**/
+function handleSubmit(event){
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   event.stopPropagation()
-
   validateInputs()
-
-// if(fname[0].value == ""){
-//   console.log("puta")
-//   document.getElementsByClassName("name")[0].parentElement.css("border-color", "red")
-//   // fname.addClass("non-valid")
-// }
-
 })
 }
 
+/**
+* GET INPUTS VALUE AND CHECK BLANK FIELDS
+**/
 function validateInputs(){
-  const name = fname.value.trim()
-  const surname = surname.value.trim()
-  const prefix = prefix.value.trim()
-  const phone = phone.value.trim()
-  const email = email.value.trim()
-  const date = date.value.trim()
-  const start_date = start_time.value.trim()
-  const party_size =party_size.value.trim()
+  // get the values from inputs
+  const nameValue = fname.value.trim()
+  const lastNameValue = surname.value.trim()
+  const prefixValue = prefix.value.trim()
+  const phoneValue = phone.value.trim()
+  const emailValue = email.value.trim()
+  const dateValue = date.value.trim()
+  const start_timeValue = start_time.value.trim()
+  // const party_sizeValue =party_size.value.trim()
 
-  if(name == ""){
+    if(nameValue === ""){
+      setErrorForBlank(fname, "Field cannot be blank");
+    }else{
+      checkLength(fname)
+    }
+  
+    if(lastNameValue === ""){
+      setErrorForBlank(surname, "Field cannot be blank");
+    }else{
+      checkLength(surname)
+    }
 
-    setErrorFor(input, "Field cannot be blank")
+    if(phoneValue === ""){
+      setErrorForBlank(phone, "Field cannot be blank");
+    }else{
+      checkLength(phone)
+    }
+
+    if(emailValue === ""){
+      setErrorForBlank(email, "Field cannot be blank");
+    }else{
+      emailValidation(emailValue)
+    }
+
+    if(dateValue === ""){
+      date.className = "form-control error"
+    }else{
+      dateValidation(dateValue)
+    }
+
+    if(start_timeValue === ""){
+      date.className = "form-control error"
+    }else{
+      // timeValidation(dateValue)
+    }
+
+}
+
+/**
+ * 
+ * VALIDATE DATE TO BOOK ON A DATE AHEAD
+ */
+function dateValidation(dateValue){
+  console.log(dateValue)
+  const month = parseInt(dateValue.slice(5,7));
+  const day = parseInt(dateValue.slice(8,10));
+  const year = parseInt(dateValue.slice(0,4));
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentDay = currentDate.getDate();
+
+  if(year >= currentYear){
+    if(month >= currentMonth){
+      if(day >= currentDay){
+        console.log("PERFECT");
+        setSuccessFor(date)
+      }else{
+        setErrorFor(date, "Day must be ahead")
+      }
+    }else{
+      setErrorFor(date, "Month must be ahead")
+    }
+  }else{
+    setErrorFor(date, "Year must be ahead")
+  }
+}
+
+/**
+ * 
+ * EMAIL FORMAT VALIDATION 
+ */
+function emailValidation(emailValue){
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if(emailValue.match(validRegex)){
+    setSuccessFor(email)
+  }else{
+    document.querySelector(".emailError").innerText = "Enter a valid email"
+  }
+}
+
+
+/**
+ * 
+ * VALIDATE INPUTS LENGTH 
+ */
+function checkLength(input){
+
+  const userData =  input.getAttribute("id");
+  const userDataValue = input.value.trim();
+
+  if(userData == "name" && userDataValue.length <= 2){
+    document.querySelector(".nameError").innerText = "Must contain min 3 characters"
+  }
+  else if(userData == "l_name" && userDataValue.length <= 2){
+    document.querySelector(".lastNameError").innerText = "Must contain min 3 characters"
+  }
+  else if(userData == "phone" && userDataValue.length <=7 || userDataValue.length >=11){
+    document.querySelector(".phoneError").innerText = "Must from 8 to 9 numbers"
   }
   else{
     setSuccessFor(input)
   }
 
-
 }
+
+/**
+ * ERROR WARNINGS AN MESSAGES
+ */
+function setErrorFor(anything, message){
+  anything.className = "form-control error"
+  document.querySelector(".dateError").innerText = message;
+}
+
+/**
+ * ERROR WARNING AN MESSAGE FOR BLANK INPUTS
+ */
+function setErrorForBlank(input, message){
+  input.className = "form-control error"
+  input.setAttribute("placeholder", message)
+}
+
+/**
+ * TURNS BORDER INPUT INTO GREEN IF DATA PASSES VALIDATION
+ */
+function setSuccessFor(input){
+  input.className = "form-control success"
+}
+
 
 // function validation(event) {
 
