02a41e26a14ac161f292ea483669a2e6eb5b7532
JavierBL89 <notri80@gmail.com>
2022-05-12 13:33:15 +0000

    Availability section design and layout updated

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -6,7 +6,10 @@ jQuery(function(){
     $(document).on("mouseover", function(){
       $(".headding h1").addClass("hover");
       });
-
+    
+      // $(".availability-button").on("click", function(){
+      //   $(".availabilty-container").slideDown("slow")
+      // })
   });
 
 
