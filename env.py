1af91ba979503c40001bd454994c07878fbbd705
JavierBL89 <notri80@gmail.com>
2022-09-06 13:48:32 +0000

    Gitignore is now working

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://yumitqkrrwqoeg:e388ced0d80e67eecbb4da09a832593e4e4bc1a75703c5cdbf0b3d07c3deeb68@ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/d5957htb0olcl0"
-os.environ["SECRET_KEY"] = "cachundalamadre"
-os.environ["CLOUDINARY_URL"] = "cloudinary://192782277256831:WyVfKoaHZXS1QXxgrGv_0fgxieI@dthlibbj7"
-os.environ["HEROKU_POSTGRESQL_MAROON_URL"] = "postgres://fnhdutzjoujizq:0823dbdee21f5961615b0198060ac5c2169159f0c3467c9bbfe8ca63a356dbad@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dfh2or16dl2i7"
-os.environ["MAPS_API_KEY"] = "AIzaSyAcWcFqwTSIkXjl0B9a8i4J6aVpIWkWk6c"
