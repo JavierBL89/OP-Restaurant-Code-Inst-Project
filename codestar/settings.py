cac5424cbada978ec270c3885bcd0d3a2a0355cf
JavierBL89 <notri80@gmail.com>
2022-09-08 01:41:59 +0000

    Collectstatic

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://one-pico-restaurant.herokuapp.com/',
+ALLOWED_HOSTS = ['https://one-pico-restaurant.herokuapp.com',
                  'localhost', '127.0.0.1:8000']
 
 
