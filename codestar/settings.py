f144bc223a3298cdd75ab67972db714f6056ee10
JavierBL89 <notri80@gmail.com>
2022-09-08 02:39:53 +0000

    Updated heroku URL ALLOWED HOSTS

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,9 +31,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://oryo-poxo-restaurant.herokuapp.com/', 'localhost', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['.herokuapp.com/', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
