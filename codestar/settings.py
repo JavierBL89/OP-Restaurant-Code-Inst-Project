97a5fa214b0f8b1ac8b8f937e25ff4cb66fecf6a
JavierBL89 <notri80@gmail.com>
2022-09-16 15:32:53 +0000

    New superuser created

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+# new superuser created
 
 ALLOWED_HOSTS = ['oryo-poxo-restaurant.herokuapp.com', 'localhost', '127.0.0.1:8000']
 
