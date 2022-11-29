9519337440f538fd0b71624039432f8bb9549807
JavierBL89 <notri80@gmail.com>
2022-11-29 01:02:19 +0000

    Fixed identation error in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -37,9 +37,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['oryo-poxo.onrender.com', 'localhost', '127.0.0.1:8000']
 
 # Add Render.com URL to allowed hosts
- RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
- if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
